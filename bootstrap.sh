c265bd4d398d1213636c05e3d061783c39f1ef26
michael stone <michaelstone.js@gmail.com>
2017-11-20 15:53:37 +0000

    Extend bootstrap script to be fully automated
    * Installs git
    * Downloads this repo from github
    * Runs ansible-playbook

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,19 @@
 
 sudo apt update
 sudo apt install software-properties-common
+
+# Install old git from main repository - latest version will be installed in ansible script
+sudo apt install git
+
+# Clone setup repository with ansible scripts
+cd ~
+git clone https://github.com/michaelstone/local-setup.git
+
+# Install ansible
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible
+
+# Run ansible
+cd local-setup
+ansible-playbook master.yml -K -i HOSTS 
